1e19d608d38d46719fa5cefe233465e24c22dae8
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-06-03 21:24:17 +0530

    hw question for thursday

---
+++ homework/3_june_2021.py
@@ -0,0 +1,11 @@
+# %%
+# Question 1:
+# Write a program that reads a string and displays the longest substring of
+# the given string having just the consonants.
+
+string = input("What is the input string? ")
+
+# %%
+# Question 2:
+# WAP that reads a string and then prints a string that capitalizes every
+# alternate character. Example: `pasSion` becomes `pAsSiOn`.
