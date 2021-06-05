88fd0bbade1900d92d5217bbc27797ef79c77c01
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-06-05 15:44:24 +0000

    add HW for 6 june

---
+++ homework/6_june_2021.py
@@ -0,0 +1,25 @@
+# %% [markdown]
+# # Question 1:
+# Show the first n elements of the fibonacci series
+#
+# Example:
+#   - input: 7
+#   - output: 0 1 1 2 3 5 8
+
+# %% [markdown]
+# # Question 2:
+# WAP to print the factorial of n.
+#
+# Example:
+#   - input: 5
+#   - output: 120
+
+# %% [markdown]
+# # Question 3:
+# WAP to input the value of `x` and `n` and print the sum
+# of the following series.
+# 1 + x<sup>1</sup> + x<sup>2</sup> + x<sup>3</sup> + .... + x<sup>n</sup>
+#
+# Example:
+#   - input: 2, 3
+#   - output: 15
