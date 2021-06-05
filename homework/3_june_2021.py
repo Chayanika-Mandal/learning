457daba2d2de490a017e3358091225fbed087057
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-06-05 15:44:16 +0000

    complete HW of 3 june

--- homework/3_june_2021.py
+++ homework/3_june_2021.py
@@ -3,22 +3,42 @@
 # Write a program that reads a string and displays the longest substring of
 # the given string having just the consonants.
 
-string = input("What is the input string? ")
-mylist = string.split()
-mysecondlist = []
-for element in mylist:
-    if "a" and "e" and "i" and "o" and "u" not in element:
-        mysecondlist.append(element)
-mythirdlist = []
-for substring in mysecondlist:
-    mythirdlist.append(len(substring))
-mythirdlist = mythirdlist.sort()
-for component in mysecondlist:
-    if len(component) == mythirdlist[len(mythirdlist) - 1]:
-        print(component)
+# string = input("What is the input string? ")
 
+string = "myh Ello nae is faeiou BRT I ghy"
 
+list_of_consonants = list()  # []
 
+temporary = ""
+for index, character in enumerate(string):
+    if character == " ":
+        continue
+    if character.lower() not in ["a", "e", "i", "o", "u"] and not character.isdigit():
+        temporary += character
+        if index == len(string) - 1:
+            list_of_consonants.append(temporary)
+    else:
+        if temporary:
+            list_of_consonants.append(temporary)
+        temporary = ""
+
+# method 1: is capable of giving only one answer
+# %%
+length = 0
+required_index = 0
+for index, substring in enumerate(list_of_consonants):
+    if len(substring) > length:
+        required_index = index
+        length = len(substring)
+
+print(f"One possible output is: {list_of_consonants[required_index]}")
+
+# method 2: is capable of giving multiple answers
+# %%
+list_of_consonants.sort(key=len, reverse=True)
+for substring in list_of_consonants:
+    if len(substring) == len(list_of_consonants[1]):
+        print(f"One possible output is: {substring}")
 
 # %%
 # Question 2:
@@ -29,11 +49,10 @@ slice1 = mystring[::2]
 slice2 = mystring[1::2]
 slice3 = slice1.swapcase()
 print(slice3)
-newstring = ("")
-for index in range(0, len(slice1)-1):
-    newstring += slice3[index]
-    newstring += slice2[index]
-if len(mystring)%2 == 1:
+newstring = ""
+for required_index in range(0, len(slice1) - 1):
+    newstring += slice3[required_index]
+    newstring += slice2[required_index]
+if len(mystring) % 2 == 1:
     newstring += slice3[len(slice3) - 1]
 print(newstring)
-# %%
