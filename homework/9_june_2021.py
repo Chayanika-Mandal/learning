5c4bfd629ad7787ed7fb9a8217bc07d23315952b
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-06-09 15:37:50 +0000

    hw questions for 9 june

---
+++ homework/9_june_2021.py
@@ -0,0 +1,24 @@
+# %% [markdown]
+# # Question 1
+# WAF to interchange the last two elements of an interable.
+#
+# 1. Input: `['1', 34, 'hello']`
+#
+#    Output: `['hello', 34, '1']`
+#
+# 2. Input: `'hello world'`
+#
+#    Output: `'dello worlh'`
+
+# 3. Input: `(1, 2, 3, 5)`
+#
+#    Output: `(5, 2, 3, 1)`
+
+# %% [markdown]
+# # Question 2
+# WAF to find the number of even and odd elements in a list of
+# integers.
+#
+# Input: `[1, 2, 3, 4, 7]`
+#
+# Output: `Even: 2, Odd: 3`
