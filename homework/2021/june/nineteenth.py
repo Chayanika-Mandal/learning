530f87b022e1f4417ddc079cd58169dfb612578b
Chayanika-Mandal <83910917+Chayanika-Mandal@users.noreply.github.com>
2021-06-20 07:51:51 -0700

    Update nineteenth.py

--- homework/2021/june/nineteenth.py
+++ homework/2021/june/nineteenth.py
@@ -1,3 +1,4 @@
+from typing import Dict
 import pytest
 
 
@@ -5,10 +6,27 @@ import pytest
 # # Question 1:
 # WAF to return the frequency of each character in a string.
 def function_1(string):
-    pass
+    def function_2(my_list):
+        if type(my_list) is not list:
+            raise TypeError(f"Expected type {list}, got {type(my_list)}")
+        else:
+            new_list = []
+            for character in my_list:
+                if character not in new_list:
+                    new_list.append(character)
+            return new_list
+
+    string = list(string)
+    function_2(string)
+    my_dict = dict()
+    for character in function_2(string):
+        my_dict[character] = string.count(character)
+    return my_dict
 
 
-@pytest.mark.xfail
+function_1("Smooth like butter.")
+
+
 def test_function_1():
     assert function_1("Hello") == {"H": 1, "e": 1, "l": 2, "o": 1}
     assert function_1("YAY 2") == {"Y": 2, "A": 1, "2": 1, " ": 1}
@@ -18,8 +36,6 @@ def test_function_1():
 # # Question 2:
 # WAF to create a third dictionary from two dictionaries having common keys.
 # in a way so that values of common keys are added in the third dictionary.
-def function_2(dict1, dict2):
-    pass
 
 
 @pytest.mark.xfail
@@ -37,6 +53,7 @@ def function_3(dict1):
     pass
 
 
+@pytest.mark.xfail
 def test_function_3():
     assert function_3({31: 111, 22: 222, 43: 455, 25: 555}) == (555, 455)
     assert function_3({1131: 111, 222: 222, 453: 555, 25: 555}) == (555, 555)
