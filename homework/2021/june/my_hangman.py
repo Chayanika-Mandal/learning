9eb4ebfe8c1c77b8d90d8c4a09a2806358dee05c
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-06-30 15:53:25 +0000

    refine implementation

--- homework/2021/june/my_hangman.py
+++ homework/2021/june/my_hangman.py
@@ -1,4 +1,21 @@
 import random
+from typing import List
+
+
+def get_representation(word: List[str]):
+    """
+    When `word` is ['h', 'e', '', 'l', 'l', '']
+    then output will be 'h e _ l l _'
+    """
+    string = ""
+    for character in word:
+        if character != "":
+            string += character
+        else:
+            string += "_"
+    string = " ".join(list(string))
+    return string
+
 
 my_list = [
     "atrocious",
@@ -12,35 +29,27 @@ my_list = [
     "cosmopolitan",
 ]
 
-random_word = random.choice(my_list)
-my_string = ""
-for x in range(0, len(random_word)):
-    my_string += "_ "
-print(my_string)
-n = int(input("Enter the number of guesses you want to make: "))
-my_new_list = []
-for element in my_string:
-    my_new_list.append(element)
+random_word = list(random.choice(my_list))
+
+guessed = ["" for _ in range(0, len(random_word))]
+
+total_guesses = int(input("Enter the number of guesses you want to make: "))
+
 wrong_guess = 0
-while wrong_guess < n:
-    letter = input("Enter your guess: ")
+
+while wrong_guess < total_guesses:
+    letter = input("\nEnter your guess: ")
     if letter in random_word:
         for index, character in enumerate(random_word):
             if character == letter:
-                my_new_list[2 * index] = letter
+                guessed[index] = letter
     else:
         wrong_guess += 1
         print(f"You have done {wrong_guess} incorrect guesses till now.")
-    x = ""
-    for p in my_new_list:
-        x += p
-    print(x)
-    my_list_2 = []
-    for character in random_word:
-        my_list_2.append(character)
-        my_list_2.append(" ")
-    if my_list_2 == my_new_list:
-        print("you have completed the word! Congrats")
+    print((get_representation(guessed)))
+
+    if guessed == random_word:
+        print("You have completed the word! Congrats!")
         break
 else:
     print("You have completed all your guesses. Try again.")
