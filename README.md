4871d176e771727bad082883e723a949e4717c0a
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-07-04 15:42:04 +0000

    hw for file handling

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # learning
- A repository which contains all my codes that I am learning.
+
+A repository which contains all my codes that I am learning.
---
+++ homework/2021/july/fourth.py
@@ -0,0 +1,16 @@
+# %% [markdown]
+# # Question 1:
+# WAP to append a user input string to the end of a file.
+
+# %% [markdown]
+# # Question 2:
+# WAP to read a file and then write the contents of that file in the reversed
+# order to the same file.
+
+# %% [markdown]
+# # Question 3:
+# You have to convert the contents of `fourth.txt` to a python list.
+
+# %% [markdown]
+# # Question 4:
+# You have to write the result of FizzBuzz game to a file called `fizzbuzz.txt`
