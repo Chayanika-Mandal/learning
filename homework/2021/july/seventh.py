f5217b1e33dbb84d9d7e8f17864a50be293f2e17
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-07-07 15:35:26 +0000

    hw for 7th july

---
+++ homework/2021/july/seventh.py
@@ -0,0 +1,23 @@
+# %% [markdown]
+# # Question 1:
+# WAP to count the frequency of each word in a file.
+
+# %% [markdown]
+# # Question 2:
+# WAP to choose a random word from a file.
+
+# %% [markdown]
+# # Question 3:
+# WAP to combine each word from first list with the corresponding word in
+# second list.
+#
+# Input:
+# first = ["hello", "hi", "how"]
+# first = ["key", "mouse", "pop", "wire", "wifi"]
+#
+# Output:
+# hellokey
+# himouse
+# howpop
+# wire
+# wifi
