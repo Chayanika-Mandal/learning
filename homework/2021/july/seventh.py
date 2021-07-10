814e16acf6a9ae0ff7d197cc090686782e747dc0
Chayanika-Mandal <83910917+Chayanika-Mandal@users.noreply.github.com>
2021-07-10 07:35:55 -0700

    Update seventh.py

--- homework/2021/july/seventh.py
+++ homework/2021/july/seventh.py
@@ -1,10 +1,35 @@
 # %% [markdown]
 # # Question 1:
 # WAP to count the frequency of each word in a file.
-
+with open("seventh.txt", "r") as file:
+    contents = file.read()
+    contents_list = contents.split(" ")
+    new_contents_list = []
+    for content in contents_list:
+        if "." in content:
+            content = content.split(".")
+        if str(content).isalpha():
+            new_contents_list.append(content)
+        else:
+            for character in content:
+                new_word = ""
+                if str(character).isalpha():
+                    new_word += character
+            new_contents_list.append(content)
+    my_dict = {}
+    for character in contents_list:
+        my_dict[character] = contents_list.count(character)
+        print(my_dict)
 # %% [markdown]
 # # Question 2:
 # WAP to choose a random word from a file.
+import random
+
+with open("seventh.txt", "r") as file:
+    contents = file.read()
+    contents_list = contents.split(" ")
+    random_word = list(random.choice(contents_list))
+    print(random_word)
 
 # %% [markdown]
 # # Question 3:
@@ -21,3 +46,23 @@
 # howpop
 # wire
 # wifi
+first = ["hello", "hi", "how"]
+second = ["key", "mouse", "pop", "wire", "wifi"]
+my_list = []
+for index, word in enumerate(first):
+    if index < len(second):
+        string = word + second[index]
+        my_list.append(string)
+    else:
+        my_list.append(word)
+for index, word in enumerate(second):
+    if index < len(first):
+        string = first[index] + word
+    else:
+        string = word
+    if string not in my_list:
+        my_list.append(string)
+for element in my_list:
+    print(element)
+
+# %%
