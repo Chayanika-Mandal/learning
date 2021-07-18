c4905bdb3a0aecb26a6ab617d0da21126150a622
Chayanika-Mandal <83910917+Chayanika-Mandal@users.noreply.github.com>
2021-07-18 08:41:57 -0700

    Create seventeenth.py

---
+++ homework/2021/july/seventeenth.py
@@ -0,0 +1,24 @@
+x = int(input("enter num"))
+y = int(input("enter num"))
+
+
+class Parent:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def get_value(self):
+        print(self.a ** self.b)
+
+
+class Child(Parent):
+    def get_value(self):
+        super().get_value()
+        num = 1
+        for integer in range(1, self.a + 1):
+            num *= integer
+        print(num)
+
+
+child = Child(x, y)
+child.get_value()
