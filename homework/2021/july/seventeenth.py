30726dab5875fd538f7770bde496eb0a1aeb85f4
Chayanika-Mandal <83910917+Chayanika-Mandal@users.noreply.github.com>
2021-07-21 09:01:24 -0700

    Update seventeenth.py

--- homework/2021/july/seventeenth.py
+++ homework/2021/july/seventeenth.py
@@ -13,7 +13,7 @@ class Parent:
 
 class Child(Parent):
     def get_value(self):
-        super().get_value()
+        super().get_value()  # super(Child, self).get_value()
         num = 1
         for integer in range(1, self.a + 1):
             num *= integer
