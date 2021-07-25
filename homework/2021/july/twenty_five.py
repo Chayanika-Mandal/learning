af99cb3c88aedff4c8c5821b6719bdbc5cd44dc3
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-07-25 15:40:01 +0000

    hw for 25 jul

---
+++ homework/2021/july/twenty_five.py
@@ -0,0 +1,23 @@
+# %%
+# Question 1:
+# Traverse a nested list and print all elements.
+names = [
+    "Adam",
+    [
+        "Bob",
+        [
+            "Chet",
+            "Cat",
+        ],
+        "Barb",
+        "Bert",
+    ],
+    "Alex",
+    ["Bea", "Bill"],
+    "Ann",
+]
+
+# %%
+# Question 2:
+# FInd the sum of all the elements of a nested list.
+numbers = [1, 2, [3, 4], [5, 6]]
